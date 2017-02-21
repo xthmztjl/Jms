cd82ffb612252b00a1bdcefae72bb5b56836484f
xthmztjl <m15227670235@163.com>
2017-02-21 16:06:35 +0800

    Add files via upload

---
+++ SendMsg_webchinese.java
@@ -0,0 +1,37 @@
+package com.jms;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+public class SendMsg_webchinese {
+
+	public static void main(String[] args) throws Exception {
+
+		HttpClient client = new HttpClient();
+		PostMethod post = new PostMethod("http://gbk.sms.webchinese.cn");
+		post.addRequestHeader("Content-Type",
+				"application/x-www-form-urlencoded;charset=gbk");// 在头文件中设置转码
+		NameValuePair[] data = { new NameValuePair("Uid", "xthmztjl"),
+				new NameValuePair("Key", "5a02ee4e1632a8666a93"),
+				new NameValuePair("smsMob", "15227670235"),
+				new NameValuePair("smsText", "【签名内容:测试短信功能。】") };
+		post.setRequestBody(data);
+
+		client.executeMethod(post);
+		Header[] headers = post.getResponseHeaders();
+		int statusCode = post.getStatusCode();
+		System.out.println("statusCode:" + statusCode);
+		for (Header h : headers) {
+			System.out.println(h.toString());
+		}
+		String result = new String(post.getResponseBodyAsString().getBytes(
+				"gbk"));
+		System.out.println(result);
+
+		post.releaseConnection();
+
+	}
+
+}
